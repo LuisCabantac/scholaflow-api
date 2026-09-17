b796dbe3d0f034aa84eada709683ad81101e75c8
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-09-18 03:16:11 +0800

    feat: add session table migration

---
+++ internal/adapters/postgresql/migrations/00002_session.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS public."session" (
+    id TEXT NOT NULL,
+    token TEXT NOT NULL UNIQUE,
+    user_id TEXT NOT NULL REFERENCES public."user"(id) ON DELETE CASCADE,
+    expires_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    ip_address TEXT,
+    user_agent TEXT,
+
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS public."session";
