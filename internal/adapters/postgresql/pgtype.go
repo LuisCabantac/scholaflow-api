a916cb840845d6bbb4d9457d2a97d7b05315abca
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-09-21 02:04:28 +0800

    chore: add pg-related value converter utils

---
+++ internal/adapters/postgresql/pgtype.go
@@ -0,0 +1,31 @@
+package postgresql
+
+import (
+	"time"
+
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+func TextFromPtr(s *string) pgtype.Text {
+	if s == nil {
+		return pgtype.Text{Valid: false}
+	}
+	return pgtype.Text{
+		String: *s,
+		Valid:  true,
+	}
+}
+
+func PtrFromText(t pgtype.Text) *string {
+	if !t.Valid {
+		return nil
+	}
+	return &t.String
+}
+
+func TimestampFromTime(t time.Time) pgtype.Timestamp {
+	return pgtype.Timestamp{
+		Time:  t,
+		Valid: true,
+	}
+}
