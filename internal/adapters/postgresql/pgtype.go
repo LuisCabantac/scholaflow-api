c58cf5e94150885dce2d34c85ca2ffb4e11aa5a0
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-09-21 03:32:58 +0800

    chore: add ParseUUID util function

--- internal/adapters/postgresql/pgtype.go
+++ internal/adapters/postgresql/pgtype.go
@@ -6,6 +6,12 @@ import (
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
+func ParseUUID(s string) (pgtype.UUID, error) {
+	var u pgtype.UUID
+	err := u.Scan(s)
+	return u, err
+}
+
 func TextFromPtr(s *string) pgtype.Text {
 	if s == nil {
 		return pgtype.Text{Valid: false}
